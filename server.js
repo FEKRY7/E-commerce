94ebb3571b9d26b0a0b961453a0c668f3fe43d52
FEKRY7 <159729804+FEKRY7@users.noreply.github.com>
2024-10-10 01:57:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,65 +1,14 @@
 const express = require('express');
-
-const userRouter = require('./src/modules/auth/auth.routes.js')
-const categoryRouter = require('./src/modules/category/category.routes.js')
-const SubcategoryRouter = require('./src/modules/subcategory/subCategory.routes.js')
-const BrandRouter = require('./src/modules/brand/brand.routes.js')
-const CuponRouter = require('./src/modules/Cupon/Cupon.routes.js')
-const ProductRouter = require('./src/modules/product/product.routes.js')
-const cartRouter = require('./src/modules/cart/cart.routes.js')
-const orderRouter = require('./src/modules/order/order.routes.js')
-const reviewRouter = require('./src/modules/review/review.routes.js')
-const mongoConnection = require('./Database/dbConnection.js');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-
-dotenv.config()
 const app = express()
-mongoConnection();
-app.use(cors())
-app.use(express.json()); // Parse request bodies as JSON
+const AppRouter = require("./server.Router.js");
 
+const dotenv = require('dotenv');
+dotenv.config()
 
-// Webhook Handling
-app.use((req,res,next)=>{
- if(req.originalUrl ==="/Order/Webhook"){
-    return next()
- }
- express.json()(req,res,next)
-})
-
-  
-
-
-// Routes
-app.use(userRouter)
-app.use('/Category',categoryRouter)
-app.use('/Subcategory',SubcategoryRouter)
-app.use('/Brand',BrandRouter)
-app.use('/Cupon',CuponRouter)
-app.use('/Product',ProductRouter)
-app.use('/Cart',cartRouter)
-app.use('/Order',orderRouter)
-app.use('/Review',reviewRouter)
-
-
-
+AppRouter(app);
 
 // Set up server to listen on specified port (default to 3000)
 const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
-// 404 route
-app.use('*', (req, res) => {
-    res.status(404).json({ 'Msg': 'I Can\'t Found' });
-});
-  
-
-
-
